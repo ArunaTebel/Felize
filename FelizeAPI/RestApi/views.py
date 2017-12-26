fa08ed00124df08ad05ccd7b071c9cc705b93742
ArunaTebel <arunatebel@hotmail.com>
2017-12-27 00:53:44 +0530

    Initial integration of oAuth Toolkit + Django REST

--- FelizeAPI/RestApi/views.py
+++ FelizeAPI/RestApi/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from RestApi.serializers import UserSerializer, GroupSerializer
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+    required_scopes = ['groups']
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
